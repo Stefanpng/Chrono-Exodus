aaef1f7b5253c5ad6acf580f477073e76de6b405
Stefanpng <251063@virtualwindow.co.za>
2025-04-11 22:51:15 +0300

    Next Level setup

---
+++ Assets/HoldToLoad.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class HoldToLoad : MonoBehaviour
+{
+    public float holdDuration = 1f;
+    public Image fillCircle;
+
+    private float holdTimer = 0;
+    private bool isHolding = false;
+
+    public static event Action OnHoldComplet;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHolding)
+        {
+            holdTimer += Time.deltaTime;
+            fillCircle.fillAmount = holdTimer / holdDuration;
+            if (holdTimer >= holdDuration)
+            {
+                //Load next level
+                OnHoldComplet.Invoke();
+                ResetHold();
+            }
+        }
+    }
+
+    public void OnHold(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            isHolding = true;
+        }
+        else if (context.canceled)
+        {
+            ResetHold();
+        }
+    }
+
+    private void ResetHold()
+    {
+        isHolding = false;
+        holdTimer = 0;
+        fillCircle.fillAmount = 0;
+    }
+}
