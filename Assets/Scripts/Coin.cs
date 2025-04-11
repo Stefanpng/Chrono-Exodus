82aab7958ef24dd2368697b52e4000b8ed76523e
Stefanpng <251063@virtualwindow.co.za>
2025-04-11 19:01:06 +0300

    UI Slider setup

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,9 +1,13 @@
+using System;
 using UnityEngine;
 
 public class Coin : MonoBehaviour, IItem
 {
+    public static event Action<int> OnCoinCollect;
+    public int worth = 5;
     public void Collect()
     {
+        OnCoinCollect.Invoke(worth);
         Destroy(gameObject);
     }
 
