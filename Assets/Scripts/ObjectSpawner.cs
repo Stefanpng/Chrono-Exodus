ff14c41bb82efde1c4ccfb027c72c36bfecf766b
Stefanpng <251063@virtualwindow.co.za>
2025-04-13 21:11:05 +0300

    Save

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -26,6 +26,7 @@ public class ObjectSpawner : MonoBehaviour
     {
         GatherValidPositions();
         StartCoroutine(SpawnObjectsIfNeeded());
+        GameController.OnReset += LevelChange;
     }
 
     // Update is called once per frame
