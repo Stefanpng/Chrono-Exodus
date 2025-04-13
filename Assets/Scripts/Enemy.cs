8c62d182a3b3bd49dfe04d0b043c93b0ff293389
Stefanpng <251063@virtualwindow.co.za>
2025-04-13 18:31:13 +0300

    Player Health setup

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,6 +11,8 @@ public class Enemy : MonoBehaviour
     private bool isGrounded;
     private bool shouldJump;
 
+    public int damage = 1;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
